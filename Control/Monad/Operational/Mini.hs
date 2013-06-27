5cd96552150795f49e0aa4c9a0faedccbf690a05
fumieval <fumiexcel@gmail.com>
2013-06-27 18:36:35 +0900

    simpler implementation

--- Control/Monad/Operational/Mini.hs
+++ Control/Monad/Operational/Mini.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Rank2Types, GADTs, TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes, FlexibleInstances, MultiParamTypeClasses, GADTs #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Monad.Operational.Mini
@@ -7,20 +7,57 @@
 --
 -- Maintainer  :  Fumiaki Kinoshita <fumiexcel@gmail.com>
 -- Stability   :  experimental
--- Portability :  non-portable
+-- Portability :  RankNTypes
 --
--- Simple operational monad from a free monad
+-- Simple operational monad
 ----------------------------------------------------------------------------
 module Control.Monad.Operational.Mini (Program, interpret, module Control.Monad.Operational.Class) where
 
-import Data.Functor.Yoneda.Contravariant
-import Control.Monad.Free.Church
 import Control.Monad.Operational.Class
+import Control.Applicative
 
-type Program t = F (Yoneda t)
+infixl 1 :>>=
+
+-- | Program t is a 'Monad' that represents a sequence of imperatives.
+-- To construct imperatives, use 'singleton' :: t a -> Program t a.
+
+newtype Program t a = Program { unProgram :: forall r. (a -> r) -> (forall x. t x -> (x -> r) -> r) -> r }
+
+instance Functor (Program t) where
+    fmap f (Program m) = Program $ \p i -> m (p . f) i
+
+instance Applicative (Program t) where
+    pure a = Program $ \p _ -> p a
+    Program mf <*> Program ma = Program $ \p i -> mf (\f -> ma (p . f) i) i
+
+instance Monad (Program t) where
+    return a = Program $ \p _ -> p a
+    Program m >>= k = Program $ \p i -> m (\a -> unProgram (k a) p i) i
 
 interpret :: Monad m => (forall x. t x -> m x) -> Program t a -> m a
-interpret e (F m) = m return (\(Yoneda f t) -> e t >>= f)
+interpret e (Program m) = m return (\t f -> e t >>= f)
 
 instance Operational t (Program t) where
-    singleton = liftF . liftYoneda
+    singleton t = Program $ \p i -> i t p
+
+data SparseProgram t a where
+    Return :: a -> SparseProgram t a
+    (:>>=) :: t a -> (a -> SparseProgram t b) -> SparseProgram t b
+
+instance Functor (SparseProgram t) where
+    fmap f = go where
+        go (Return a) = Return (f a)
+        go (t :>>= k) = t :>>= go . k
+    {-# INLINE fmap #-}
+
+instance Applicative (SparseProgram t) where
+    pure = Return
+    {-# INLINE pure #-}
+    Return f <*> Return a = Return (f a)
+    mf <*> m = mf >>= \f -> fmap f m
+
+instance Monad (SparseProgram t) where
+    return = Return
+    {-# INLINE return #-}
+    Return a >>= f = f a
+    (t :>>= m) >>= k = t :>>= (>>= k) . m
